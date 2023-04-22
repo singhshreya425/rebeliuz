046707879581451b3e85b58d4df0dbe560d47830
unknown <singhshreya425@gmail.com>
2023-04-22 15:35:40 +0530

    new

--- rebback/src/index.js
+++ rebback/src/index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://singhshreya425:shreyasingh1234@cluster0.yxxvuvg.mongodb.net/Camera", {
+mongoose.connect("mongodb+srv://singhshreya425:shreyasingh1234@cluster0.yxxvuvg.mongodb.net/shreya526", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
